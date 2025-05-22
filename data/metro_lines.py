bffe48191adefd320661e5f54c8b0cc0890105b4
PP-URC <proyectoprototipico666.com@gmail.com>
2025-05-22 14:31:00 -0600

    Create metro_lines.py

---
+++ data/metro_lines.py
@@ -0,0 +1,65 @@
+metro_lines = {
+    "Linea 1": [
+        "Observatorio", "Tacubaya", "Juanacatlán", "Chapultepec", "Sevilla",
+        "Insurgentes", "Cuauhtémoc", "Balderas", "Salto del Agua", "Isabel la Católica",
+        "Pino Suárez", "Merced", "Candelaria", "San Lázaro", "Moctezuma", "Balbuena",
+        "Boulevard Pto. Aéreo", "Gómez Farías", "Zaragoza", "Pantitlán"
+    ],
+    "Linea 2": [
+        "Cuatro Caminos", "Panteones", "Tacuba", "Cuitláhuac", "Popotla", "Colegio Militar",
+        "Normal", "San Cosme", "Revolución", "Hidalgo", "Bellas Artes", "Allende",
+        "Zócalo", "Pino Suárez", "San Antonio Abad", "Chabacano", "Viaducto", "Xola",
+        "Villa de Cortés", "Nativitas", "Portales", "Ermita", "General Anaya", "Tasqueña"
+    ],
+    "Linea 3": [
+        "Indios Verdes", "Deportivo 18 de Marzo", "Potrero", "La Raza", "Tlatelolco", "Guerrero",
+        "Hidalgo", "Juárez", "Balderas", "Niños Héroes", "Hospital General", "Centro Médico",
+        "Etiopía / Plaza de la Transparencia", "Eugenia", "División del Norte", "Zapata",
+        "Coyoacán", "Viveros / Derechos Humanos", "Miguel Ángel de Quevedo", "Copilco", "Universidad"
+    ],
+    "Linea 4": [
+        "Martín Carrera", "Talismán", "Bondojito", "Consulado", "Canal del Norte", "Morelos",
+        "Fray Servando", "Candelaria", "Jamaica", "Santa Anita"
+    ],
+    "Linea 5": [
+        "Pantitlán", "Hangares", "Terminal Aérea", "Oceanía", "Aragón", "Eduardo Molina",
+        "Consulado", "Valle Gómez", "Misterios", "La Raza", "Autobuses del Norte",
+        "Instituto del Petróleo", "Politécnico"
+    ],
+    "Linea 6": [
+        "El Rosario", "Tezozómoc", "Azcapotzalco", "Ferrería", "Norte 45",
+        "Vallejo", "Instituto del Petróleo", "Lindavista", "Deportivo 18 de Marzo",
+        "La Villa-Basílica", "Martín Carrera"
+    ],
+    "Linea 7": [
+        "El Rosario", "Aquiles Serdán", "Camarones", "Refinería", "Tacuba", "San Joaquín",
+        "Polanco", "Auditorio", "Constituyentes", "Tacubaya", "San Pedro de los Pinos",
+        "San Antonio", "Mixcoac", "Barranca del Muerto"
+    ],
+    "Linea 8": [
+        "Garibaldi", "Bellas Artes", "San Juan de Letrán", "Salto del Agua", "Doctores",
+        "Obrera", "Chabacano", "La Viga", "Santa Anita", "Coyuya", "Iztacalco", "Apatlaco",
+        "Aculco", "Escuadrón 201", "Atlalilco", "Iztapalapa", "Cerro de la Estrella",
+        "UAM-I", "Constitución de 1917"
+    ],
+    "Linea 9": [
+        "Tacubaya", "Patriotismo", "Chilpancingo", "Centro Médico", "Lázaro Cárdenas", "Chabacano",
+        "Jamaica", "Mixiuhca", "Velódromo", "Ciudad Deportiva", "Puebla", "Pantitlán"
+    ],
+    "Linea A": [
+        "Pantitlán", "Agrícola Oriental", "Canal de San Juan", "Tepalcates", "Guelatao",
+        "Peñón Viejo", "Acatitla", "Santa Marta", "Los Reyes", "La Paz"
+    ],
+    "Linea B": [
+        "Buenavista", "Guerrero", "Garibaldi", "Lagunilla", "Tepito", "Morelos",
+        "San Lázaro", "Ricardo Flores Magón", "Romero Rubio", "Oceanía", "Deportivo Oceanía",
+        "Bosque de Aragón", "Villa de Aragón", "Nezahualcóyotl", "Impulsora", "Río de los Remedios",
+        "Múzquiz", "Ecatepec", "Olímpica", "Plaza Aragón", "Ciudad Azteca"
+    ],
+    "Linea 12": [
+        "Mixcoac", "Insurgentes Sur", "Hospital 20 de Noviembre", "Zapata", "Parque de los Venados",
+        "Eje Central", "Ermita", "Mexicaltzingo", "Atlalilco", "Culhuacán", "San Andrés Tomatlán",
+        "Lomas Estrella", "Calle 11", "Periférico Oriente", "Tezonco", "Olivos", "Nopalera",
+        "Zapotitlán", "Tlaltenco", "Tláhuac"
+    ]
+}
